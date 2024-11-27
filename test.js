11167333e5b4d5a055f3ce932748cbefdc1f6c6c
Linghao Zhang <starrywhuzhang@gmail.com>
2024-11-28 05:33:02 +0800

    init instance

---
+++ test.js
@@ -0,0 +1,14 @@
+import test from 'ava';
+import {readChunkSync} from 'read-chunk';
+import imageType, {minimumBytes} from './index.js';
+
+const check = async filename => {
+	const {ext} = await imageType(readChunkSync(filename, {length: minimumBytes}));
+	return ext;
+};
+
+test('main', async t => {
+	t.is(await check('fixture.png'), 'png');
+	t.is(await check('fixture.psd'), 'psd');
+	t.is(await check('fixture.avif'), 'avif');
+});
